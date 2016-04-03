53bb6e2230f714f29f9350a2efc7ad97c0371706
Tim D. Smith <git@tim-smith.us>
2016-04-02 20:40:59 -0700

    Handle incoming webhooks

--- snooze/snooze.py
+++ snooze/snooze.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import logging
 import requests
 
@@ -18,21 +20,82 @@ def github_patch(config, repo, url, data):
     return requests.patch(url, auth=auth, json=data)
 
 
+def github_get(config, repo, url):
+    repo_config = config[repo]
+    auth = requests.auth.HTTPBasicAuth(
+        repo_config["github_username"],
+        repo_config["github_token"])
+    return requests.get(url, auth=auth)
+
+
+def clear_snooze_label_if_set(config, repo, issue):
+    issue_labels = {label["name"] for label in issue.get("labels", [])}
+    if SNOOZE_LABEL not in issue_labels:
+        logging.debug(
+            "clear_snooze_label_if_set: Label not set on {}".format(
+                issue["html_url"]
+            ))
+        return False
+    issue_labels.remove(SNOOZE_LABEL)
+    r = github_patch(
+        config,
+        repo,
+        issue["url"],
+        {"labels": list(issue_labels)})
+    r.raise_for_status()
+    logging.debug(
+        "clear_snooze_label_if_set: Removed snooze label from {}".format(
+            issue["html_url"]
+        ))
+    return True
+
+
 def github_callback(config, event, message):
     if event == "issue_comment":
         # When an issue is updated, we want to clear the SNOOZE_LABEL if it's set.
         issue = message["issue"]
-        issue_labels = {label["name"] for label in issue.get("labels", [])}
-        if SNOOZE_LABEL in issue_labels:
-            issue_labels.remove(SNOOZE_LABEL)
-            r = github_patch(
-                config,
-                message["repository"]["full_name"],
-                issue["url"],
-                {"labels": list(issue_labels)})
-            r.raise_for_status()
+        logging.debug("Incoming issue hook: {}".format(issue["html_url"]))
+        return clear_snooze_label_if_set(
+            config,
+            message["repository"]["full_name"],
+            issue)
+    elif event == "pull_request":
+        logging.debug("Incoming PR hook: {}".
+                      format(message["pull_request"]["html_url"]))
+        # We should take action when a pull request is synchronized.
+        if message["action"] != "synchronize":
+            return False
+        logging.debug("Synchronize hook for PR: {}".
+                      format(message["pull_request"]["html_url"]))
+        # Fetch the matching issue object to see if SNOOZE_LABEL is set
+        r = github_get(
+            config,
+            message["repository"]["full_name"],
+            message["pull_request"]["issue_url"])
+        r.raise_for_status()
+        issue = r.json()
+        return clear_snooze_label_if_set(
+            config,
+            message["repository"]["full_name"],
+            issue)
+    elif event == "pull_request_review_comment":
+        logging.debug("Incoming PR comment hook: {}".
+                      format(message["pull_request"]["html_url"]))
+        # Fetch the matching issue object to see if SNOOZE_LABEL is set
+        r = github_get(
+            config,
+            message["repository"]["full_name"],
+            message["pull_request"]["issue_url"])
+        r.raise_for_status()
+        issue = r.json()
+        return clear_snooze_label_if_set(
+            config,
+            message["repository"]["full_name"],
+            issue)
     else:
         logging.warning("Ignoring event type {}".format(event))
+
+    return False
 
 
 def main():
--- snooze/test/test_snooze.py
+++ snooze/test/test_snooze.py
@@ -171,9 +171,82 @@ class TestGithubWebhookCallback(object):
         responses.add(
             responses.PATCH,
             "https://api.github.com/repos/baxterthehacker/public-repo/issues/2")
-        snooze.github_callback(config,
-                               "issue_comment",
-                               json.loads(github_responses.ISSUE_COMMENT))
+        r = snooze.github_callback(config,
+                                   "issue_comment",
+                                   json.loads(github_responses.SNOOZED_ISSUE_COMMENT))
+        assert r is True
+        assert len(responses.calls) == 1
+
+    @responses.activate
+    def test_issue_comment_callback_not_snoozed(self, config):
+        """Don't do anything on receiving an unsnoozed message."""
+        r = snooze.github_callback(config,
+                                   "issue_comment",
+                                   json.loads(github_responses.UNSNOOZED_ISSUE_COMMENT))
+        assert r is False
+        assert len(responses.calls) == 0
+
+    @responses.activate
+    def test_pr_synchronize_callback(self, config):
+        """Test that a snooze label is removed from PRs when a new commit is
+        pushed."""
+        responses.add(
+            responses.PATCH,
+            "https://api.github.com/repos/octocat/Hello-World/issues/1347")
+        responses.add(
+            responses.GET,
+            "https://api.github.com/repos/baxterthehacker/public-repo/issues/1",
+            body=github_responses.SNOOZED_ISSUE_GET)
+        r = snooze.github_callback(config,
+                                   "pull_request",
+                                   json.loads(github_responses.PULL_REQUEST))
+        assert r is True
+        assert len(responses.calls) == 2
+
+    @responses.activate
+    def test_pr_synchronize_callback_not_snoozed(self, config):
+        """Test that a snooze label is not removed from PRs when a new commit is
+        pushed but there is no snooze label."""
+        responses.add(
+            responses.GET,
+            "https://api.github.com/repos/baxterthehacker/public-repo/issues/1",
+            body=github_responses.UNSNOOZED_ISSUE_GET)
+        r = snooze.github_callback(config,
+                                   "pull_request",
+                                   json.loads(github_responses.PULL_REQUEST))
+        assert r is False
+        assert len(responses.calls) == 1
+
+    @responses.activate
+    def test_pr_commit_comment_callback(self, config):
+        """Test that a snooze label is removed from PRs when a new commit is
+        pushed."""
+        responses.add(
+            responses.PATCH,
+            "https://api.github.com/repos/octocat/Hello-World/issues/1347")
+        responses.add(
+            responses.GET,
+            "https://api.github.com/repos/baxterthehacker/public-repo/issues/1",
+            body=github_responses.SNOOZED_ISSUE_GET)
+        r = snooze.github_callback(config,
+                                   "pull_request_review_comment",
+                                   json.loads(github_responses.PULL_REQUEST_REVIEW_COMMENT))
+        assert r is True
+        assert len(responses.calls) == 2
+
+    @responses.activate
+    def test_pr_commit_comment_callback_not_snoozed(self, config):
+        """Test that a snooze label is not removed from PRs when a new commit is
+        pushed but there is no snooze label."""
+        responses.add(
+            responses.GET,
+            "https://api.github.com/repos/baxterthehacker/public-repo/issues/1",
+            body=github_responses.UNSNOOZED_ISSUE_GET)
+        r = snooze.github_callback(
+            config,
+            "pull_request_review_comment",
+            json.loads(github_responses.PULL_REQUEST_REVIEW_COMMENT))
+        assert r is False
         assert len(responses.calls) == 1
 
     def test_bad_callback_type_is_logged(self, config):
