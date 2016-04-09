d42d572f9097c5c475e0ca84d7ed80f03cf2eb9a
Tim D. Smith <git@tim-smith.us>
2016-04-08 23:38:23 -0700

    Induce lambda handler to log to Cloudwatch

--- snooze/lambda_handler.py
+++ snooze/lambda_handler.py
@@ -1,14 +1,21 @@
 from __future__ import absolute_import
 
 import json
+import logging
 
 from snooze.callbacks import github_callback
 from snooze.lambda_config import github_auth, snooze_label
+
+# this appears to be magical
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 def lambda_handler(event, _):
     for record in event['Records']:
         sns_message = record['Sns']
         github_event = sns_message['MessageAttributes']['X-Github-Event']['Value']
+        logger.debug("Received event type %s" % github_event)
         github_message = json.loads(sns_message['Message'])
         github_callback(github_event, github_message, github_auth, snooze_label)
